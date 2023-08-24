c46abd35c90201dbf7f78a9c6e4ea9e836ef7afd
lingyuan <lingyjava@163.com>
2023-08-24 17:28:27 +0800

    add docs

---
+++ demo/mysql.js
@@ -0,0 +1,53 @@
+// 导入 Mysql
+const mysql = require('mysql')
+// 建立数据库连接池
+const db = mysql.createPool({
+    // 数据库 ip 地址
+    host: '127.0.0.1',
+    // 账号
+    user: 'root',
+    // 密码
+    password: '123456',
+    // 库名
+    database: 'user_ly'
+})
+
+// 查询
+db.query('select * from user_member', (err, results) => {
+    if (err) {
+        console.log(err.message)
+        return ;
+    }
+    console.log(results)
+})
+
+// 插入
+const userMember = {
+    userLoginName: 'ly',
+    password: '123456',
+    salt: 'abcdefg'
+}
+const insertSqlStr = 'insert into user_member(user_login_name, password, salt) values (?, ?, ?)'
+// 使用数组形式为 ? 占位符赋值
+db.query(insertSqlStr, [userMember.userLoginName, userMember.password, userMember.salt], (err, results) => {
+    if (err) {
+        return console.log(err.message)
+    }
+    console.log(results)
+})
+
+// 全字段插入
+const insertAllSqlStr = 'insert into user_member SET ?'
+// 使用对象作为占位符的值，对象属性应与表字段对应
+db.query(insertAllSqlStr, userMember, (err, results) => {
+    // ...
+})
+
+// 修改
+const userMember2 = {
+    id: 5,
+    userLoginName: 'ly',
+    password: '456798',
+    salt: 'abcdefg'
+}
+const updateSqlStr = 'update user_member set ?  where id = ?'
---
+++ docs/8·Mysql.md
@@ -0,0 +1,14 @@
+# 8·MySQL
+
+
+
+## 安装
+
+mysql 模块是托管于 npm 上的第三方模块。提供了在 Node.js 项目中连接和操作 MySQL 数据库的能力。
+
+运行如下命令 `npm install mysql`，将 mysql 安装为项目的依赖包。
+
+## 配置
+
+在使用 mysql 模块操作 MySQL 数据库之前，必须先对 mysql 模块进行必要的配置。
+
